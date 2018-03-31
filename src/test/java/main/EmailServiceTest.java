656b68b18e83f209866a0581d8dacdbeff267f13
costia097 <adaw36909@gmail.com>
2018-03-31 18:36:45 +0300

    add email service test

--- src/test/java/main/EmailServiceTest.java
+++ src/test/java/main/EmailServiceTest.java
@@ -1,11 +1,21 @@
 package main;
 
+import main.services.EmailService;
 import main.services.SenderResolver;
 import main.services.UserService;
+import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.mockito.Mock;
 import org.springframework.mail.javamail.JavaMailSenderImpl;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+import javax.mail.MessagingException;
+import javax.mail.internet.MimeMessage;
+
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.doNothing;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
 
 @RunWith(SpringJUnit4ClassRunner.class)
 public class EmailServiceTest {
@@ -15,4 +25,18 @@ public class EmailServiceTest {
     private JavaMailSenderImpl javaMailSender;
     @Mock
     private UserService userService;
+    @Mock
+    private MimeMessage mimeMessage;
+
+    @Test
+    public void testsendEmailToPerson() throws MessagingException {
+        EmailService emailService = new EmailService(senderResolver, javaMailSender, userService);
+        String to = "to";
+        String subject = "subject";
+        String text = "text";
+        doNothing().when(senderResolver).resolveSender(any(),any());
+        when(javaMailSender.createMimeMessage()).thenReturn(mimeMessage);
+        emailService.sendEmailToPerson(to, subject, text);
+        verify(javaMailSender).send(mimeMessage);
+    }
 }
